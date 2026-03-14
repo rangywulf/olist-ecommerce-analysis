de0d107906056540eb8bc596dbee20f01750a17f
rangywulf <82498366+rangywulf@users.noreply.github.com>
2026-03-13 21:21:58 -0600

    Add revenue by category query and results

---
+++ queries/04_revenue_analysis.sql
@@ -0,0 +1,10 @@
+SELECT
+    t.product_category_name_english,
+    ROUND(SUM(o.price), 2)AS total_revenue,
+    COUNT(o.order_id) AS total_orders
+FROM products p
+INNER JOIN product_category_translation t ON p.product_category_name = t.product_category_name
+INNER JOIN order_items o ON p.product_id = o.product_id
+GROUP BY t.product_category_name_english
+ORDER BY total_revenue DESC
+LIMIT 15;
